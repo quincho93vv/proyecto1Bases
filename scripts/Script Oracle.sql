4888426c2c8564232a79c767276120dd8a90ea08
quincho93vv <jjose93v@gmail.com>
2015-04-10 22:30:33 -0600

    Subo los scripts para cada base, sin procedimientos almacenados

---
+++ scripts/Script Oracle.sql
@@ -0,0 +1,51 @@
+-----------------------------------------------------------
+-- Base de Datos	: Proyecto 1
+-- Modelo Version	: 1
+-- Fecha			: 10/04/2015
+-- DBA's			:	*Alonso Navarro Camareno
+--						*Jose Joaquin Vasquez Chaves
+--						*Mario Romero Sandoval
+-- Servidor		    : Oracle 11g Express Edition
+-----------------------------------------------------------
+
+CREATE TABLE Vendedores(
+	codigo int,
+	nombre varchar(30),
+	totalVendido float,
+	CONSTRAINT PKVendedores PRIMARY KEY(codigo)
+);
+
+CREATE TABLE Clientes(
+	codigo int,
+	nombre varchar(30),
+	vendedor int,
+	CONSTRAINT PKClientes PRIMARY KEY(codigo),
+	CONSTRAINT FKClientes FOREIGN KEY(vendedor) REFERENCES Vendedores
+);
+
+CREATE TABLE Facturas(
+	numero int,
+	tipo varchar(2),
+	fecha date,
+	total float,
+	cliente int,
+	CONSTRAINT PKFacturas PRIMARY KEY(numero),
+	CONSTRAINT FKFacturas FOREIGN KEY(cliente) REFERENCES Clientes
+);
+
+CREATE TABLE Productos(
+	codigo int,
+	nombre varchar(30),
+	precio float,
+	CONSTRAINT PKProductos PRIMARY KEY(codigo)
+);
+
+CREATE TABLE Detalles(
+	producto int,
+	factura int, 
+	cantidad int,
+	total float,
+	CONSTRAINT PKDetalles PRIMARY KEY(producto, factura),
+	CONSTRAINT FKDetalles1 FOREIGN KEY(producto) REFERENCES Productos,
+	CONSTRAINT FKDetalles2 FOREIGN KEY(factura) REFERENCES Facturas
+);
